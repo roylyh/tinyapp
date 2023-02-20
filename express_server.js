575617abf01d9189dbbfd5549d1658f927eeeea7
roylyh <roy.louyihui@gmail.com>
2023-02-20 15:43:31 -0800

    register a handler on the root path

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const { PORT } = require("./constants");
+const express = require("express");
+const app = express();
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
