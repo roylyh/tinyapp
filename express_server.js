ec764662e95e93aa5f4bd82dc6aaae66f669ee52
roylyh <roy.louyihui@gmail.com>
2023-02-20 16:37:47 -0800

    add ejs file

--- express_server.js
+++ express_server.js
@@ -18,19 +18,16 @@ app.get("/urls.json", (req, res) => {
 }
 );
 
+app.get("/urls", (req, res) => {
+  const templateVars = {urls:urlDatabase};
+  res.render("urls_index", templateVars);
+}
+);
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 }
 );
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
