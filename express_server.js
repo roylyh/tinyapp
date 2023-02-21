ed31bfb7d0eee4066a705047c05062f46ba0391d
roylyh <roy.louyihui@gmail.com>
2023-02-20 16:27:10 -0800

    set view engine ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const { PORT } = require("./constants");
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
@@ -15,6 +17,20 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 }
 );
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+}
+);
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+ 
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
