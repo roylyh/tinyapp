47cff8665041261394df7e1ebaa179b62c94929c
roylyh <roy.louyihui@gmail.com>
2023-02-21 23:57:35 -0800

    Displaying Username with cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const { PORT } = require("./constants");
 const { generateRandomString } = require("./util");
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.set("view engine", "ejs");
@@ -12,6 +13,7 @@ const urlDatabase = {
 
 // parse the dody
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello! Welcome to URL shorting application.");
@@ -27,7 +29,7 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls:urlDatabase};
+  const templateVars = {urls:urlDatabase, username: req.cookies.username, };
   res.render("urls_index", templateVars);
 }
 );
@@ -40,11 +42,12 @@ app.post("/urls", (req, res) => {
 );
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies.username, };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies.username, };
   res.render("urls_show", templateVars);
 });
 
