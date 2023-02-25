5d52f01f6550fd49bf5df3fb0aa6b94cbb5a0a0f
roylyh <roy.louyihui@gmail.com>
2023-02-24 21:41:51 -0800

    add comment

--- helpers.js
+++ helpers.js
@@ -1,9 +1,11 @@
+// generate a random string, length = 6 default
 const generateRandomString = function(length = 6) {
   return Math.random()
     .toString(20)
     .substring(2, length + 2);
 };
 
+// get a obj whose userID equals id from urlDatabase
 const urlsForUser = function(id, urlDatabase) {
   const keys = Object.keys(urlDatabase).filter(
     (key) => urlDatabase[key].userID === id
@@ -15,6 +17,7 @@ const urlsForUser = function(id, urlDatabase) {
   return result;
 };
 
+// find the user in the database with email, if not found return undefined
 const getUserByEmail = function(email, database) {
   for (let user in database) {
     if (database[user].email === email) {
